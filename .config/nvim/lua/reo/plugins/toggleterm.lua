5e2a95a8ea36235fc31476c61b78746b2d8dc243
Redooyyy <rwarieal@gmail.com>
2026-01-09 21:34:01 +0600

    added floating terminal plugin

---
+++ .config/nvim/lua/reo/plugins/toggleterm.lua
@@ -0,0 +1,15 @@
+return {
+  "Redooyyy/floatbar-nvim",
+  config = function()
+    -- Enable true colors globally (optional if not in init.lua)
+    vim.o.termguicolors = true
+
+    -- Setup plugin with default config
+    require("floatbar").setup({
+      width = 0.7, -- 70% width
+      height = 0.7, -- 70% height
+      winblend = 0, -- transparency
+      border = "rounded", -- rounded border
+    })
+  end,
+}
