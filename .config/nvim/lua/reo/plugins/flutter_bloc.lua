0b6724bf0bc6ba7770b213c5f1b2d0752d3c01ae
Redooyyy <rwarieal@gmail.com>
2026-01-13 21:50:15 +0600

    added bloc-flutter for flutter dev

---
+++ .config/nvim/lua/reo/plugins/flutter_bloc.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "wa11breaker/flutter-bloc.nvim",
+    config = function()
+      require("flutter-bloc").setup({
+        bloc_type = "default",
+        use_sealed_classes = false,
+      })
+
+      vim.keymap.set("n", "<Leader>bb", function()
+        require("flutter-bloc").create_bloc()
+      end, { desc = "Create Flutter Bloc" })
+
+      vim.keymap.set("n", "<Leader>bc", function()
+        require("flutter-bloc").create_cubit()
+      end, { desc = "Create Flutter Cubit" })
+    end,
+  },
+
+  {
+    "RobertPietraru/bloc.nvim",
+    dependencies = {
+      {
+        "nvimtools/none-ls.nvim",
+        name = "null-ls.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+      },
+    },
+    config = function()
+      require("bloc").setup()
+    end,
+  },
+}
