5ad8c0efe9f62a0949a048fc108d973445efab55
Redooyyy <rwarieal@gmail.com>
2026-01-18 19:57:41 +0600

    updated flutter_deps plugin

--- .config/nvim/lua/reo/plugins/flutter_deps.lua
+++ .config/nvim/lua/reo/plugins/flutter_deps.lua
@@ -1,12 +1,11 @@
 return {
   "Redooyyy/flutter-deps.nvim",
   dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
+    "nvim-lua/plenary.nvim", -- async jobs
+    -- optionally: "stevearc/dressing.nvim" for nicer vim.ui.select UI
   },
   config = function()
-    require("flutter_deps").setup()
-
+    -- no setup() call, because init.lua doesn’t define it
     vim.keymap.set("n", "<leader>pd", function()
       require("flutter_deps").add_dependency()
     end, { desc = "Add pub.dev dependency" })
