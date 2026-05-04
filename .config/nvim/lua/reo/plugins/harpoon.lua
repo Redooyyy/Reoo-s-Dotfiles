dfb2cedeb7c4f133492fb2d2553b178524e33948
Redooyyy <rwarieal@gmail.com>
2026-05-04 23:50:19 +0600

    nothing

--- .config/nvim/lua/reo/plugins/harpoon.lua
+++ .config/nvim/lua/reo/plugins/harpoon.lua
@@ -5,6 +5,7 @@ return {
     local mark = require("harpoon.mark")
     local ui = require("harpoon.ui")
 
+    --just saving streaks -_-
     --harpoon (swap between files)--
     vim.keymap.set("n", "<leader>a", mark.add_file)
     vim.keymap.set("n", "<C-a>", ui.toggle_quick_menu)
